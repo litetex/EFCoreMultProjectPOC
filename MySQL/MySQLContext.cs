d24fae636fe9df1f65eaaffb0e1d53cabcf8f5db
litetex <litetex.dev@gmail.com>
2019-07-28 13:48:04 +0200

    Added MySQL
    * for testing reasons

---
+++ MySQL/MySQLContext.cs
@@ -0,0 +1,16 @@
+﻿using Base;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace MySQL
+{
+   public class MySQLContext : Context
+   {
+      protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+      {
+         base.OnConfiguring(optionsBuilder);
+
+         optionsBuilder.UseMySQL("server=localhost;database=test;user=test;password=test");
+      }
+   }
+}
